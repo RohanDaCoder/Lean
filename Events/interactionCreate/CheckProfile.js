840932b6cdc10747188bff9a67082c612a2ff7dd
Rohan <rohanbmgo2010@gmail.com>
2024-05-04 16:48:27 +0600

    update CheckProfile.js

--- Events/interactionCreate/CheckProfile.js
+++ Events/interactionCreate/CheckProfile.js
@@ -2,21 +2,23 @@ const DB = require("simple-json-db");
 const fs = require("fs");
 
 module.exports = (interaction, client) => {
-  let dbPath = `../../Database/${interaction.user.id}.json`
-  fs.appendFile(dbPath, '{}');
-  const profile = new DB(dbPath);
-  if (profile.has("userID")) return;
-  const defaultProfile = {
-    name: interaction.user.username,
-    userID: interaction.user.id,
-    wallet: 0,
-    bank: 0,
-    inventory: []
-  };
+  let dbPath = `../../Database/${interaction.user.id}.json`;
+  fs.appendFile(dbPath, '{}', function(err) {
+    if(err) return interaction.reply(`Failed To Check Balance. \nError: ${e.message}`);
+    const profile = new DB(dbPath);
+    if (profile.has("userID")) return;
+    const defaultProfile = {
+      name: interaction.user.username,
+      userID: interaction.user.id,
+      wallet: 0,
+      bank: 0,
+      inventory: []
+    };
 
-  profile.set("name", defaultProfile.name);
-  profile.set("userID", defaultProfile.userID);
-  profile.set("wallet", defaultProfile.wallet);
-  profile.set("bank", defaultProfile.bank);
-  profile.set("inventory", defaultProfile.inventory);
+    profile.set("name", defaultProfile.name);
+    profile.set("userID", defaultProfile.userID);
+    profile.set("wallet", defaultProfile.wallet);
+    profile.set("bank", defaultProfile.bank);
+    profile.set("inventory", defaultProfile.inventory);
+  });
 };
