eef1571ca9a014b0c42023ffb1b834f05712e657
Rohan <rohanbmgo2010@gmail.com>
2024-05-04 17:01:19 +0600

    update CheckProfile.js

--- Events/interactionCreate/CheckProfile.js
+++ Events/interactionCreate/CheckProfile.js
@@ -1,10 +1,11 @@
 const DB = require("simple-json-db");
 const fs = require("fs");
+const path = require("path");
 
 module.exports = (interaction, client) => {
-  let dbPath = `../../Database/${interaction.user.id}.json`;
-  fs.appendFile(dbPath, '{}', function(e) {
-    if(e) return interaction.channel.send(`Failed To Check Balance. \nError: ${e.message}`);
+  let dbPath = path.join(__dirname, `../../Database/${interaction.user.id}.json`);
+  fs.writeFile(dbPath, '{}', 'utf8', function(e) {
+    if (e) return interaction.channel.send(`Failed To Check Balance. \nError: ${e.message}`);
     const profile = new DB(dbPath);
     if (profile.has("userID")) return;
     const defaultProfile = {
