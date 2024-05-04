988ceda965c765d6361e05b41cfc3b37a0916552
Rohan <rohanbmgo2010@gmail.com>
2024-05-04 11:30:44 +0600

    update balance.js

--- Commands/economy/balance.js
+++ Commands/economy/balance.js
@@ -25,7 +25,7 @@ module.exports = {
     }
 
     try {
-      const { username, walletFormatted, bankFormatted } = await GetBalance(targetUser.id);
+      const { username, walletFormatted, bankFormatted } = await GetBalance(targetUser.id, client);
 
       const balanceEmbed = new EmbedBuilder()
         .setColor('#0099ff')
