c532c3e65ab9bdab03128214e8b8525bde1aab74
Rohan <rohanbmgo2010@gmail.com>
2024-05-04 22:03:14 +0600

    update stats.js

---
+++ Commands/utility/stats.js
@@ -0,0 +1,82 @@
+const {
+  SlashCommandBuilder
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("stats")
+    .setDescription("Display bot statistics"),
+
+  run: async ({
+    client,
+    interaction
+  }) => {
+    const {
+      EmbedBuilder
+    } = require("discord.js");
+
+    // Fetching values from the client
+    const ping = client.ws.ping;
+    const uptime = client.uptime / 1000 / 60;
+    const memoryUsage = (process.memoryUsage()
+        .heapUsed / 1024 / 1024)
+      .toFixed(2);
+    const guildsCount = client.guilds.cache.size;
+    const usersCount = client.users.cache.size;
+    const apiLatency = Date.now() - interaction.createdTimestamp;
+    const version = require("./package.json")
+      .version;
+    const discordJSVersion = require("discord.js")
+      .version;
+    const nodeVersion = process.version;
+
+    // Creating the embed
+    const embed = new EmbedBuilder()
+      .setColor(6200464)
+      .setTitle("Stats from `Lean`")
+      .setAuthor("Stats", client.user.defaultAvatarURL())
+      .setTimestamp()
+      .addFields({
+        name: "<a:a_online:1007918346374746133> Ping",
+        value: `┕ \`${ping}ms\``,
+        inline: true
+      }, {
+        name: ":clock1: Uptime",
+        value: `┕ \`${uptime}m\``,
+        inline: true
+      }, {
+        name: ":file_cabinet: Memory",
+        value: `┕ \`${memoryUsage}mb\``,
+        inline: true
+      }, {
+        name: ":homes: Servers",
+        value: `┕ \`${guildsCount}\``,
+        inline: true
+      }, {
+        name: ":busts_in_silhouette: Users",
+        value: `┕ \`${usersCount}\``,
+        inline: true
+      }, {
+        name: ":control_knobs: API Latency",
+        value: `┕ \`${apiLatency}ms\``,
+        inline: true
+      }, {
+        name: ":robot: Version",
+        value: `┕ \`${version}\``,
+        inline: true
+      }, {
+        name: ":blue_book: Discord.js",
+        value: `┕ \`${discordJSVersion}\``,
+        inline: true
+      }, {
+        name: ":green_book: Node",
+        value: `┕ \`${nodeVersion}\``,
+        inline: true
+      });
+
+    // Sending the embed
+    await interaction.reply({
+      embeds: [embed]
+    });
+  }
+};
